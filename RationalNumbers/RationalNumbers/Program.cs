9003f410ea97ed3566b40633a090a7c124ef50a4
battila98 <55810364+battila98@users.noreply.github.com>
2020-11-17 19:29:49 +0100

    Közös óra után (combos óra)

--- RationalNumbers/RationalNumbers/Program.cs
+++ RationalNumbers/RationalNumbers/Program.cs
@@ -8,13 +8,26 @@ namespace RationalNumbers
 {
     class Program
     {
-        public void Bekeres()
-        {
-           string nevezo = Console.ReadLine(Convert.ToInt32());
-        }
+        
 
         static void Main(string[] args)
         {
+
+
+            var rational1 = new Rational(1, 2);
+            var rational2 = new Rational(1, 2);
+            Console.WriteLine($"{rational1 == rational2} should be true");
+
+            /*var rational1 = new Rational(1, 2);
+            var rational2 = new Rational(3, 2);
+            Console.WriteLine($"{rational1 < rational2} should be true");
+
+            var rational1 = new Rational(1, 2);
+            var rational2 = new Rational(1, 2);
+            Console.WriteLine($"{rational1 + rational2} should be 1 instead of 1 / 1");
+            */
+
+
         }
     }
 }
--- RationalNumbers/RationalNumbers/Rational.cs
+++ RationalNumbers/RationalNumbers/Rational.cs
@@ -6,38 +6,71 @@ using System.Threading.Tasks;
 
 namespace RationalNumbers
 {
-    struct Rational
+    struct Rational : IComparable<Rational>, IEquatable<Rational>
     {
-        int numerator;       // számláló
-        int denominator;      // nevező
-        public int getNumerator
+        private int numerator;       // számláló
+        private int denominator;     // nevező -> alsó lol
+
+        public Rational(int numerator, int denominator)
         {
-            get
+
+            if (denominator == 0)
             {
-                return this.numerator;
+                throw new System.InvalidOperationException("Nullával nem osztunk, heh!");
             }
-            set
-            {
-                if (numerator == 0)
-                {
-                    throw new System.InvalidOperationException("Nullával nem osztunk, heh!");
-                }
-            }
+
+            int gcd = Gcd(numerator, denominator);
+            this.numerator = numerator / gcd;
+            this.denominator = denominator / gcd;
         }
-        public int getDenominator
+        public static Rational operator +(Rational x, Rational y)
         {
-            get
-            {
-                return this.denominator;
-            }
-            set
-            {
-                if (denominator == 0)
-                {
-                    throw new System.InvalidOperationException("Nullával nem osztunk, heh!");
-                }
-            }
+            return new Rational(x.numerator * y.denominator + y.numerator * x.denominator, x.denominator * y.denominator);
         }
-        
+        public static Rational operator -(Rational x, Rational y)
+        {
+            return new Rational(x.numerator * y.denominator - y.numerator * x.denominator, x.denominator * y.denominator);
+        }
+        public static Rational operator *(Rational x, Rational y)
+        {
+            return new Rational(x.numerator * y.numerator, x.denominator * y.denominator);
+        }
+        public static Rational operator /(Rational x, Rational y) //buzi
+        {
+            return x * new Rational(y.denominator, y.numerator);
+        }
+        public static bool operator ==(Rational x, Rational y)
+        {
+            return x.Equals(y);
+        }
+        public static bool operator !=(Rational x, Rational y) // ==-t kell így is definiálni, mert csak (tudni mi van ha nem ==)
+        {
+            return !(x == y);
+        }
+        private static int Gcd(int x, int y) => y == 0 ? x : Gcd(y, x % y);
+
+        public int CompareTo(Rational other) // ha a bal oldal < mint OTHER akkor -1; Ha == akkor 0; Ha nagyobb akkor 1;
+        {
+            /*if (this.Equals(other))
+            {
+                return 0;
+            }
+            else if ()
+            {
+
+            }
+            else
+            {
+
+            }*/
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(Rational other)
+        {
+            return this.numerator == other.numerator && this.denominator == other.denominator; //boolean 
+        }
+
+        public override string ToString() => denominator == 1 ? $"{numerator}" : $"{numerator} / {denominator}"; // metódus név => if ... ? (akkor)... : (else) ...
     }
 }
